ae46e9dd60d5bbfa2556fc230da07acf72649c96
Leon ROUX <lroux@e2r12p21.42.fr>
2018-11-18 18:41:30 +0100

    v0.1 - Struct

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: lroux <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 15:21:40 by lroux             #+#    #+#             */
-/*   Updated: 2018/11/18 15:25:00 by lroux            ###   ########.fr       */
+/*   Updated: 2018/11/18 18:32:21 by lroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 # define FILLIT_H
 
 # include <libft.h>
-# include <gnl.h>
+
+typedef struct		s_fill {
+	size_t			h;
+	size_t			w;
+	char			letter;
+	struct s_fill	*next;
+}					t_fill;
+
+# define RCF_FAIL -1
+# define RCF_KEK 0
+# define RCF_EOF 1
+
+void			usage(char *name);
+void			error(void);
+void			clean(void);
+int				rcf(t_fill **list, char *filename);
 
 #endif
